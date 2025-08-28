71fd769dd669666b66e4a13ffd3d674f8cb0cfb0
veronicaronda <vero.ronda@protonmail.com>
2025-08-28 10:58:34 +0200

    separating server actions in a file

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,38 +1,9 @@
 import PostForm from "@/components/post-form";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+
 
 
 export default function NewPostPage() {
-  async function createPost(prevState,formData){
-    "use server";
-    const title = formData.get('title')
-    const image = formData.get('image')
-    const content = formData.get('content')
-    let errors = []
 
-    if (!title || title.trim().length === 0){
-      errors.push('Title is invalid.')
-    }
-    if (!content || content.trim().length === 0){
-      errors.push('Content is invalid.')
-    }
-    if (!image || image.size === 0){
-      errors.push('Image is invalid.')
-    }
-
-    if (errors.length>0){
-      return {errors}
-    }
-    await storePost({
-      imageUrl:'',
-      title,
-      content,
-      userId: 1
-    })
-
-    redirect('/feed')
-  }
    return(
     <PostForm action={createPost} />
    )
